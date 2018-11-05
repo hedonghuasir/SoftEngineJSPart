fd375c18fb204b5864b4bda35ea3a95bc6e58d5a
hedonghua <hedonghua@hoolai.com>
2018-11-05 17:40:51 +0800

    第二部分画旋转的正方体

--- SoftEngine.js
+++ SoftEngine.js
@@ -7,17 +7,18 @@ var SoftEngine;
         }
         return Camera;
     })();
-    SoftEngine.Camera = Camera;    
+    SoftEngine.Camera = Camera;
     var Mesh = (function () {
-        function Mesh(name, verticesCount) {
+        function Mesh(name, verticesCount, facesCount) {
             this.name = name;
             this.Vertices = new Array(verticesCount);
-            this.Rotation = BABYLON.Vector3.Zero();
-            this.Position = BABYLON.Vector3.Zero();
+            this.Faces = new Array(facesCount);
+            this.Rotation = new BABYLON.Vector3(0, 0, 0);
+            this.Position = new BABYLON.Vector3(0, 0, 0);
         }
         return Mesh;
     })();
-    SoftEngine.Mesh = Mesh;    
+    SoftEngine.Mesh = Mesh;
     var Device = (function () {
         function Device(canvas) {
             this.workingCanvas = canvas;
@@ -51,6 +52,42 @@ var SoftEngine;
                 this.putPixel(point.x, point.y, new BABYLON.Color4(1, 1, 0, 1));
             }
         };
+        Device.prototype.drawLine = function (point0, point1) {
+            var dist = point1.subtract(point0).length();
+            if(dist < 2) {
+                return;
+            }
+            var middlePoint = point0.add((point1.subtract(point0)).scale(0.5));
+            this.drawPoint(middlePoint);
+            this.drawLine(point0, middlePoint);
+            this.drawLine(middlePoint, point1);
+        };
+        Device.prototype.drawBline = function (point0, point1) {
+            var x0 = point0.x >> 0;
+            var y0 = point0.y >> 0;
+            var x1 = point1.x >> 0;
+            var y1 = point1.y >> 0;
+            var dx = Math.abs(x1 - x0);
+            var dy = Math.abs(y1 - y0);
+            var sx = (x0 < x1) ? 1 : -1;
+            var sy = (y0 < y1) ? 1 : -1;
+            var err = dx - dy;
+            while(true) {
+                this.drawPoint(new BABYLON.Vector2(x0, y0));
+                if((x0 == x1) && (y0 == y1)) {
+                    break;
+                }
+                var e2 = 2 * err;
+                if(e2 > -dy) {
+                    err -= dy;
+                    x0 += sx;
+                }
+                if(e2 < dx) {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
+        };
         Device.prototype.render = function (camera, meshes) {
             var viewMatrix = BABYLON.Matrix.LookAtLH(camera.Position, camera.Target, BABYLON.Vector3.Up());
             var projectionMatrix = BABYLON.Matrix.PerspectiveFovLH(0.78, this.workingWidth / this.workingHeight, 0.01, 1.0);
@@ -58,13 +95,21 @@ var SoftEngine;
                 var cMesh = meshes[index];
                 var worldMatrix = BABYLON.Matrix.RotationYawPitchRoll(cMesh.Rotation.y, cMesh.Rotation.x, cMesh.Rotation.z).multiply(BABYLON.Matrix.Translation(cMesh.Position.x, cMesh.Position.y, cMesh.Position.z));
                 var transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
-                for(var indexVertices = 0; indexVertices < cMesh.Vertices.length; indexVertices++) {
-                    var projectedPoint = this.project(cMesh.Vertices[indexVertices], transformMatrix);
-                    this.drawPoint(projectedPoint);
+                for(var indexFaces = 0; indexFaces < cMesh.Faces.length; indexFaces++) {
+                    var currentFace = cMesh.Faces[indexFaces];
+                    var vertexA = cMesh.Vertices[currentFace.A];
+                    var vertexB = cMesh.Vertices[currentFace.B];
+                    var vertexC = cMesh.Vertices[currentFace.C];
+                    var pixelA = this.project(vertexA, transformMatrix);
+                    var pixelB = this.project(vertexB, transformMatrix);
+                    var pixelC = this.project(vertexC, transformMatrix);
+                    this.drawBline(pixelA, pixelB);
+                    this.drawBline(pixelB, pixelC);
+                    this.drawBline(pixelC, pixelA);
                 }
             }
         };
         return Device;
     })();
-    SoftEngine.Device = Device;    
+    SoftEngine.Device = Device;
 })(SoftEngine || (SoftEngine = {}));
